dbdac180c5f959104c2ad4b84d5f5d5677c3034d
xt-dev-alan <atobar@xetechs.com>
2020-04-24 09:22:15 -0600

    added module for reports

--- report_xetechs0/__manifest__.py
+++ report_xetechs0/__manifest__.py
@@ -3,20 +3,20 @@
     'name': "report_xetechs0",
 
     'summary': """
-        Sale order quotation with no prices""",
+        Sale order quotation with no prices, includes 2 reports: products only, total only""",
 
     'description': """
         module developed to add a quotation report that does not display the prices or the total of the quantity, this could bug
         be used show what type of services/products a company has offer""",
 
-    'author': "XETECHS",
+    'author': "XETECHS -->atobar@xetechs.com",
     'website': "http://www.xetechs.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sale',
-    'version': '2.1',
+    'version': '2.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],
